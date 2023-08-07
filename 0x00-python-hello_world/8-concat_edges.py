651c8c4c98c4da49cf53a045e05aa70c6aaa619e
H11TM4N <jeremiahbrownie@gmail.com>
2023-08-07 14:42:34 +0100

    concat edges task

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = ' '.join(str.split()[5:7]) + ' '+ ' '.join(str.split()[12:13]) +' ' + ' '.join(str.split()[:1])
+str = ' '.join(str.split()[5:7]) + ' '+ \
+        ' '.join(str.split()[12:13]) +' ' + ' '.join(str.split()[:1])
 print(str)
