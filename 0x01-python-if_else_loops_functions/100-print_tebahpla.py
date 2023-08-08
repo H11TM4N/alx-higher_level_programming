1d5436fbb32bc95a871d2858eebb50269cf0ff1b
H11TM4N <jeremiahbrownie@gmail.com>
2023-08-08 22:48:29 +0100

    print tebahpla (task 14)

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 == 0:
-        print(chr(i).upper(), end='')
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    if i == 0:
+        i = 32
     else:
-        print(chr(i), end='')
+        i = 0
