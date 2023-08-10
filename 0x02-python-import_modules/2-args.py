ca6b003088b16303b311a5c36fd653a647b69458
H11TM4N <jeremiahbrownie@gmail.com>
2023-08-10 20:37:42 +0100

    args (task 2)

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-input("Enter aomthig: ")
+if __name__ == "__main__":
+    import sys
+    
+    len = len(sys.argv) - 1
+
+    if len == 0:
+        print("{} arguments.".format(len))
+    elif len == 1:
+        print("{} argument:".format(len))
+        print("{}: {}".format(len, sys.argv[1]))
+    else:
+        print("{} arguments:".format(len))
+        for i in range(len):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
