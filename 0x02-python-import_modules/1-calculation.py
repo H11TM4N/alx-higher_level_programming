216b24be7854a90381551ea20fdb041f20886f58
H11TM4N <jeremiahbrownie@gmail.com>
2023-08-10 19:53:22 +0100

    forgot the if name == main

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, add(a, b))) # addition
-    print("{} - {} = {}".format(a, b, sub(a, b))) # subtraction
-    print("{} * {} = {}".format(a, b, mul(a, b))) # multiply
-    print("{} / {} = {}".format(a, b, div(a, b))) # division
+    print("{} + {} = {}".format(a, b, add(a, b)))  # addition
+    print("{} - {} = {}".format(a, b, sub(a, b)))  # subtraction
+    print("{} * {} = {}".format(a, b, mul(a, b)))  # multiply
+    print("{} / {} = {}".format(a, b, div(a, b)))  # division
