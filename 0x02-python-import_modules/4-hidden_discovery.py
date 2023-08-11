93aecefd243a276f8996aedb407a2e0f08ee1ab0
H11TM4N <jeremiahbrownie@gmail.com>
2023-08-11 12:37:48 +0100

    hidden discovery (task 4)

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import sys
+    import hidden_4
 
-    result = 0
-    for i in range(len(sys.argv) - 1):
-        result += int(sys.argv[i + 1])
-    print("{}".format(result))
+    names = dir(hidden_4)
+    for n in names:
+        if n[:2] != "__":
+            print(n)
