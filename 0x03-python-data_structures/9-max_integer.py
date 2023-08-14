1b3eea0ac4dc257c224d54286061c2cf65c09d80
H11TM4N <jeremiahbrownie@gmail.com>
2023-08-14 17:22:06 +0100

    Find the max5 (task 9)

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    high = my_list[0]
+    max = my_list[0]
     for i in my_list:
-        if i > high:
-            high = i
+        if i > max:
+            max = i
     if len(my_list) == 0:
         return None
-    else:
-        return high
+    return max
