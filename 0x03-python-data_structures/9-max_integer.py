f54338ae18965a14eae5b8ed734e8e58fefb01bf
H11TM4N <jeremiahbrownie@gmail.com>
2023-08-14 17:25:22 +0100

    Find the max6 (task 9)

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = my_list[0]
-    for i in my_list:
-        if i > max:
-            max = i
-    if len(my_list) == 0:
+    if not my_list:
         return None
-    return max
+
+    max_num = my_list[0]
+    for i in my_list:
+        if i > max_num:
+            max_num = i
+    return max_num
