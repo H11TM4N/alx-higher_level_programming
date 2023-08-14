fbc5b25a433bd1923f75682ccd4079d98ac9794a
H11TM4N <jeremiahbrownie@gmail.com>
2023-08-14 17:16:46 +0100

    Find the max4 (task 9)

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = my_list[0]
+    high = my_list[0]
     for i in my_list:
-        if i > max:
-            max = i
+        if i > high:
+            high = i
     if len(my_list) == 0:
         return None
     else:
-        return int(max)
+        return high
