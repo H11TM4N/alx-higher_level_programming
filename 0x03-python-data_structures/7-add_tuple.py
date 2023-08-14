392fff3d4c4da611032dd08c01acf2947dc9ad0d
H11TM4N <jeremiahbrownie@gmail.com>
2023-08-14 16:45:19 +0100

    Tuples addition(1) (task 6)

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,7 +3,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     first = tuple_a + (0, 0)[:2 - len(tuple_a)]
     second = tuple_b + (0, 0)[:2 - len(tuple_b)]
 
-    # Calculate the sum of corresponding elements
+    # Calculate sum of corresponding elements
     sum_first = first[0] + second[0]
     sum_second = first[1] + second[1]
 
