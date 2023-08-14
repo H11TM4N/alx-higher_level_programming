47490cf08e54611896595a7ca13828d1b7c4ec42
H11TM4N <jeremiahbrownie@gmail.com>
2023-08-14 16:44:12 +0100

    List of lists = matrix(4) (task 6)

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    first = tuple_a + (0, 0)[:2 - len(tuple_a)]
+    second = tuple_b + (0, 0)[:2 - len(tuple_b)]
+
+    # Calculate the sum of corresponding elements
+    sum_first = first[0] + second[0]
+    sum_second = first[1] + second[1]
+
+    # Return the result as a tuple
+    result = (sum_first, sum_second)
+    return result
