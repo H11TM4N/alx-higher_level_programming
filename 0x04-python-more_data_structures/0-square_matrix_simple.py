b9490331a631a4547599a4ec288541f877244783
H11TM4N <jeremiahbrownie@gmail.com>
2023-08-16 12:38:46 +0100

    Squared simple (task 0)

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    m_copy = matrix.copy()
+    for row in range(len(matrix)):
+        m_copy[row] = list(map(lambda x: x**2, matrix[row]))
+    return m_copy
