1c0a0652baace7e7479e5a75f90a7a5d78089475
H11TM4N <jeremiahbrownie@gmail.com>
2023-08-29 15:30:18 +0100

    Access and update private attribute (task 4)

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3
+"""A class Square that defines a square by: (based on 3-square.py)"""
+
+
+class Square:
+    """A Square"""
+
+    def __init__(self, size=0):
+        """Initialization of a new square
+
+    Args:
+        size (int): The first parameter.
+
+    Returns:
+        None
+    """
+        self.size = size
+
+    @property
+    def size(self):
+        """property"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """property setter"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+            A public instance method.
+        """
+        return self.__size * self.__size
