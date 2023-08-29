52651ae2739e02f60727146a28cdc9d84ea62930
H11TM4N <jeremiahbrownie@gmail.com>
2023-08-29 14:34:16 +0100

    Size Validation (task 2)

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""A class Square that defines a square by: (based on 1-square.py)"""
+
+
+class Square:
+    """A Square"""
+
+    def __init__(self, size=0):
+        """Initialization of a new square
+
+    Args:
+        size (int): The first parameter.
+
+    Returns:
+        None
+    """
+        self.__size = size
+        try:
+            size >= 0
+        except (ValueError, TypeError):
+            print("Error")
