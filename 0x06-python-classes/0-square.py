8ba94af65682345c1e7f285fa144ec550d34279e
H11TM4N <jeremiahbrownie@gmail.com>
2023-08-29 14:08:05 +0100

    update

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""Write an empty class Square that defines a square"""
+"""Write an empty class Square that defines a square:"""
 
 
 class Square:
-    """A square"""
+    """
+    An empty class Square that defines a square.
+
+    Attributes:
+        square: A class attribute used to store square objects.
+
+    Methods:
+        None.
+    """
     pass
