00d79400e1725beb54e6a4706d8e1c136e7c1404
H11TM4N <jeremiahbrownie@gmail.com>
2023-09-12 14:37:16 +0100

    Task 8 solution

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This ***8 defines a Python class-to-JSON function"""
+
+
+def class_to_json(obj):
+    """Returns the dictionary representation of a simple data structure"""
+    return obj.__dict__
