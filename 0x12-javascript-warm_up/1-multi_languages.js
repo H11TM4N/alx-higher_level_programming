d7bdda0731988bdeec5426cee88f086e58ee66f3
H11TM4N <jeremiahbrownie@gmail.com>
2023-11-14 18:21:24 +0100

    error fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const String1 = 'C is fun';
-const String2 = 'Python is cool';
-const String3 = 'JavaScript is amazing';
+const myString1 = 'C is fun';
+const myString2 = 'Python is cool';
+const myString3 = 'JavaScript is amazing';
 
-console.log(String1);
-console.log(String2);
-console.log(String3);
+console.log(myString1);
+console.log(myString2);
+console.log(myString3);
